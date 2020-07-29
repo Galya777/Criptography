7aafcb96c1a6aa5b180ff25c1be41dd7a29b008a
Galya777 <58034004+Galya777@users.noreply.github.com>
2020-07-29 16:15:12 +0300

    Add files via upload

---
+++ Diffie_Hellman.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream> 
+#include<math.h> 
+
+// Функцията a ^ b mod P 
+long long int power(long long int a, long long int b,
+	long long int P)
+{
+	if (b == 1) {
+		return a;
+	}
+	else {
+		return (((long long int)pow(a, b)) % P);
+	}
+}
+
+int main()
+{
+	long long int P, G, x, a, y, b, ka, kb;
+
+	std::cout << "Enter prime number for P"<<std::endl;
+	std::cin >> P;
+	
+	std::cout<<"The value of P : "<< P<<std::endl;
+
+	std::cout << "Enter prime number for G" << std::endl;
+	std::cin >> G;
+	std::cout<<"The value of G : "<<G<<std::endl;
+
+	std::cout << "Choose the private key a" << std::endl;
+	std::cin >> a;
+	std::cout<<"The private key a : "<< a;
+	x = power(G, a, P); // gets the generated key 
+
+	std::cout << "Choose the private key b" << std::endl;
+	std::cin >> b;
+	std::cout << "The private key b : " << a;
+	y = power(G, b, P); // gets the generated key 
+
+	// Generating the secret key after the exchange 
+		// of keys 
+	ka = power(y, a, P); // Secret key for a
+	kb = power(x, b, P); // Secret key for b
+
+	std::cout<<"Secret key for the Alice is : "<< ka;
+	std::cout<<"Secret Key for the Bob is :"<< kb;
+
+	return 0;
+}
---
+++ RSA_example.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+// намиране на най-голям общ делител
+int gcd(int a, int b) {
+	int t;
+	while (1) {
+		t = a % b;
+		if (t == 0)
+			return b;
+		a = b;
+		b = t;
+	}
+}
+int main() {
+	double p;
+	double q;
+	cout << "Enter two prime numbers:" << endl;
+	cin >> p >> q;
+	double n = p * q;
+	double track;
+	double phi = (p - 1) * (q - 1);
+	double encription_key;
+	cin >> encription_key;
+	//for checking that 1 < e < phi(n) and gcd(e, phi(n)) = 1; i.e., e and phi(n) are coprime.
+	while (encription_key < phi) {
+		track = gcd(encription_key, phi);
+		if (track == 1)
+			break;
+		else
+			encription_key++;
+	}
+	//private key
+	//d stands for decrypt
+	//choosing d such that it satisfies d*e = 1 mod phi
+	double d1 = 1 / encription_key;
+	double decription_key = fmod(d1, phi);
+	double message;
+	cout << "Enter your message!" << endl;
+	cin >> message;
+	double c = pow(message, encription_key); //encrypt the message
+	double m = pow(c, decription_key);
+	c = fmod(c, n);
+	m = fmod(m, n);
+	cout << "Original Message = " << message;
+	cout << "\n" << "p = " << p;
+	cout << "\n" << "q = " << q;
+	cout << "\n" << "n = pq = " << n;
+	cout << "\n" << "phi = " << phi;
+	cout << "\n" << "e = " << encription_key;
+	cout << "\n" << "d = " << decription_key;
+	cout << "\n" << "Encrypted message = " << c;
+	cout << "\n" << "Decrypted message = " << m;
+	return 0;
+}
