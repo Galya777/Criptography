39daba937d7a20ca187ee14e4357c48a0b55312d
Galya <58034004+Galya777@users.noreply.github.com>
2021-10-30 11:31:20 +0300

    Add files via upload

---
+++ Ceasar Sypher/CeasarShyper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ceasar_Shyper
+{
+    class CeasarShyper
+    {
+        private int shift;
+        public CeasarShyper(int lenght)
+        {
+            shift = lenght;
+        }
+        public int ShiftLength { get { return shift; } set { shift = Math.Abs(value) < 26 ? value : 3; } }
+        public string Encrypt(string plainText)
+        {
+            char[] plaintextChars = plainText.ToCharArray();
+            char[] cipherTextCh = new char[plaintextChars.Length];
+            for (int i = 0; i < plaintextChars.Length; i++)
+            {
+                cipherTextCh[i] = (char)('A' + (plaintextChars[i] - 'A' + shift + 26) % 26);
+            }
+            return new string(cipherTextCh);
+        }
+        public string Decrypt(string cipherText)
+        {
+            char[] ciphertextChars = cipherText.ToCharArray();
+            char[] plainTextCh = new char[ciphertextChars.Length];
+            for (int i = 0; i < ciphertextChars.Length; i++)
+            {
+                plainTextCh[i] = (char)('A' + (ciphertextChars[i] - 'A' - shift + 26) % 26);
+            }
+
+            return new string(plainTextCh);
+        }
+    }
+}
---
+++ Ceasar Sypher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ceasar_Shyper
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            CeasarShyper ceasar = new CeasarShyper(3);
+            var code = ceasar.Encrypt("TOY");
+            Console.WriteLine($"Encrypt TOY: {code}");
+            var plain = ceasar.Decrypt(code);
+            Console.WriteLine($"Decrypt TOY: {plain}");
+        }
+    }
+}
