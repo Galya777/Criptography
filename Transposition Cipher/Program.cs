0f87c81efad04f403acefe34fe41697834cfab7c
Galya <58034004+Galya777@users.noreply.github.com>
2021-12-01 00:55:15 +0200

    Add files via upload

---
+++ Transposition Cipher/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transposition_Cipher
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            
+                String plainText;
+                String keyText;
+                String command;
+              
+                Console.WriteLine("Will you encrypt or decrypt?");
+                command = Console.ReadLine();
+                if (command=="encrypt")
+                {
+                    Console.WriteLine("Enter plain text: ");
+                    plainText = Console.ReadLine();
+                    Console.WriteLine("Enter key text: ");
+                    keyText = Console.ReadLine();
+
+                TransCipher transCipher = new TransCipher(plainText, keyText);
+                Console.WriteLine(transCipher.encrypt(plainText, keyText, '.'));
+
+                }
+                else if (command=="decrypt")
+                {
+                    Console.WriteLine("Enter plain text: ");
+                    plainText = Console.ReadLine();
+                Console.WriteLine("Enter key text: ");
+                    keyText = Console.ReadLine();
+                TransCipher transCipher = new TransCipher(plainText, keyText);
+                    Console.WriteLine(transCipher.decrypt(plainText,keyText));
+
+                }
+
+            }
+        
+    }
+}
