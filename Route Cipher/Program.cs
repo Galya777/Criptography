c295d43f5e0c2f511a69629c4878d9b1abd1cda1
Galya <58034004+Galya777@users.noreply.github.com>
2021-12-12 00:37:00 +0200

    Add files via upload

---
+++ Route Cipher/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteCipher
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string command;
+            int key;
+            string text;
+      
+            Console.WriteLine("Will you encrypt or decrypt?");
+            command = Console.ReadLine();
+            if (command.Equals("encrypt"))
+            {
+                Console.WriteLine("Enter key: ");
+                key = Console.Read();
+                Console.WriteLine("Enter plain text: ");
+                text = Console.ReadLine();
+
+
+                RouteCipher encr = new RouteCipher(key);
+                Console.WriteLine(encr.encrypt(text));
+
+            }
+            else if (command.Equals("decrypt"))
+            {
+                Console.WriteLine("Enter text: ");
+                text = Console.ReadLine();
+                Console.WriteLine("Enter key: ");
+                key = Console.Read();
+
+                RouteCipher encr = new RouteCipher(key);
+                Console.WriteLine(encr.decrypt(text));
+
+            }
+        }
+    }
+    
+}
---
+++ Route Cipher/RouteCipher.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteCipher
+{
+    public class RouteCipher
+    {
+        private int cipher;
+
+        public RouteCipher(int cipher)
+        {
+            this.cipher = cipher;
+        }
+
+        public int getCipher()
+        {
+            return cipher;
+        }
+
+        public void setCipher(int cipher)
+        {
+            this.cipher = cipher;
+        }
+
+        public string encrypt(string plainText)
+        {
+            char[] cipherTextChars = plainText.ToCharArray();
+            char[] encrypted = new char[cipher * plainText.Length];
+            int counter = 0;
+
+            for (int rows = 2; rows <= cipher; rows++)
+            {
+                for (int repeat = 0; repeat < rows; repeat++)
+                {
+                    for (int character = repeat; character < plainText.Length; character += rows)
+                    {
+                        encrypted[counter] = cipherTextChars[character];
+                        counter++;
+                    }
+                }
+            }
+            return new string(encrypted);
+        }
+        public string decrypt(string cipherText)
+        {
+            char[] cipherTextChars = cipherText.ToCharArray();
+            char[] decrypted = new char[cipher * cipherText.Length];
+            int counter = 0;
+            for (int rows = 2; rows <= cipherText.Length; rows++)
+            {
+                int columns = (cipher) / (rows);
+                for (int i = 0; i < columns; i++)
+                {
+                    for (int increment = i; increment < cipher; increment += columns)
+                    {
+                        decrypted[counter] = cipherTextChars[increment];
+                        counter++;
+                    }
+                }
+            }
+            return new string(decrypted);
+        }
+
+    }
+}
